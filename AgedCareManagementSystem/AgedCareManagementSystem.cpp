4d6df510e18fd4e7602fa112ff0eb11c02d89005
lman888 <liamw98@hotmail.com>
2025-07-18 22:28:39 +1000

    Updated  gitignore and added a small output

--- AgedCareManagementSystem/AgedCareManagementSystem.cpp
+++ AgedCareManagementSystem/AgedCareManagementSystem.cpp
@@ -1,6 +1,8 @@
+#include <print>
 
 int main(int argc, char* argv[])
 {
-    
-    return 0;
+	std::println("Welcome to the Aged Care Management System!\n");
+	std::println("Welcome to the Aged Care Management System!\n");
+	return 0;
 }
